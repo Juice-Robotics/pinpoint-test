526224676ed95fd16772a3cee58b14b7a5c356ad
Justin Zhang <crypticrystal@gmail.com>
2024-09-10 22:08:49 -0700

    Revert "testing (has 1+5 bucket side auton)"
    This reverts commit 74558f7876964f4c4d099afdf217ac58adc80aeb.

--- IntoTheDeep/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AllianceColor.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.util;
-
-public enum AllianceColor {
-    RED,
-    BLUE,
-    BOTH
-}
--- IntoTheDeep/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MotionProfileGenerator.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.util;
-
-public class MotionProfileGenerator {
-    public static MotionProfile generateSimpleMotionProfile(double start, double end, double maxvel, double maxaccel) {
-        return new MotionProfile(start, end, maxvel, maxaccel);
-    }
-}
