0cda2dc7e195cb31eee67a2c4ceceb5084efa444
Justin Zhang <crypticrystal@gmail.com>
2024-09-10 21:38:50 -0700

    test

--- IntoTheDeep/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ IntoTheDeep/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -7,7 +7,7 @@ import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
-    publi              c static void main(String[] args) {
+    public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(750);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
